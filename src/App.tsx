2a9265b8308d948c511064caf1e62b300564a1d3
Yannick <yannick.liebnau@gmail.com>
2025-07-05 23:46:32 +0200

    improved start animation, WIP scroll effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useRef } from 'react';
 
 import { gsap } from 'gsap';
-import { ScrollTrigger, ScrollSmoother } from 'gsap/all';
+import { ScrollTrigger, ScrollSmoother, MorphSVGPlugin } from 'gsap/all';
 import { useGSAP } from '@gsap/react';
 
 import { textApparitionAnim } from './animations';
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+import "./App.css";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, MorphSVGPlugin)
 function App() {
   const container = useRef<HTMLDivElement>(null);
 
@@ -17,6 +19,25 @@ function App() {
       smoothTouch: 0.1
     })
 
+    const entries = gsap.utils.toArray(".menu-entry")
+
+    if (entries && entries.length > 0) {
+      entries.forEach(entry => {
+        const el = entry as HTMLElement;
+        const entryTop = (entries[0] as HTMLElement).getBoundingClientRect().top
+
+        gsap.to(el, {
+          x: "-500px",
+          scrollTrigger: {
+            trigger: el,
+            start: `bottom ${entryTop}`,
+            end: `${window.innerHeight} top`,
+            markers: true,
+          }
+        })
+      })
+    }
+
     textApparitionAnim('h1', 'h2', '.description');
 
   }, { scope: container })
@@ -24,18 +45,33 @@ function App() {
   return (
     <div ref={container} id="smooth-wrapper">
       <div id="smooth-content">
-        <div className="home container h-screen relative">
-          <div className="content-wrapper h-screen flex flex-col justify-center items-center">
-            <h1>Yannick Liebnau</h1>
-            <h2 className="mb-3">Web Developer</h2>
-            <div className="description relative w-100 text-center">
-              <p>JavaScript / NodeJS / React</p>
-              <p>Prestashop / WordPress</p>
+        <div className="home container h-[200svh]">
+          <div className="content-wrapper h-screen flex flex-col">
+            <div className="presentation-wrapper flex-grow-1 h-100 relative flex items-center justify-center">
+              <div className="presentation h-fit flex flex-col items-center relative">
+                <h1>Yannick Liebnau</h1>
+                <div className="subtitle-container mb-3 relative">
+                  <h2 className="subtitle">Web Developer</h2>
+                  <div className="opacity-0 line line1 w-16 h-px bg-gray-900 inline absolute top-1/2 left-0 translate-y-1/2"></div>
+                  <div className="opacity-0 line line2 w-35 h-px bg-gray-900 inline absolute top-1/2 right-0 translate-y-1/2"></div>
+                </div>
+                <div className="description relative w-100 text-center">
+                  <p>JavaScript / NodeJS / React</p>
+                  <p>Prestashop / WordPress</p>
+                </div>
+              </div>
+            </div>
+            <div className="menu-wrapper self-end">
+              <ul className="menu flex flex-col gap-5 text-right">
+                <li className="menu-entry text-xl">Projects</li>
+                <li className="menu-entry text-xl">About</li>
+                <li className="menu-entry text-xl">Contact</li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
-    </div >
+    </div>
   )
 }
 
--- src/animations.ts
+++ src/animations.ts
@@ -9,11 +9,13 @@ async function textApparitionAnim(titleElem = 'h1', subtitleElem = 'h2', descrip
     const timeline = gsap.timeline();
 
     const splitTitle = SplitText.create(titleElem, {
-      type: "chars"
+      type: "chars",
+      charsClass: "title-char++"
     });
 
     const splitSubtitle = SplitText.create(subtitleElem, {
-      type: "chars"
+      type: "words, chars",
+      wordsClass: "subtitle-word++",
     });
 
     const splitDescription = SplitText.create(descriptionElem, {
@@ -21,13 +23,19 @@ async function textApparitionAnim(titleElem = 'h1', subtitleElem = 'h2', descrip
     })
 
     timeline.addLabel('start')
-      .from(splitTitle.chars, {
-        x: -50,
+      .fromTo(splitTitle.chars, {
+        x: -20,
         opacity: 0,
-        stagger: 0.05,
-        duration: 0.8,
+      }, {
+        x: 0,
+        opacity: 1,
+        stagger: 0.07,
+        duration: 1,
         ease: "back.out"
       })
+      .to(".title-char:not(.title-char1)", {
+        x: -5,
+      }, "<1.5")
       .addLabel("Subtitle Animation")
       .fromTo(splitSubtitle.chars, {
         opacity: 0,
@@ -42,7 +50,7 @@ async function textApparitionAnim(titleElem = 'h1', subtitleElem = 'h2', descrip
         },
         duration: 2,
         stagger: 0.05,
-      }, "<1")
+      }, "<")
       .addLabel("Description animation")
       .from(splitDescription.lines, {
         opacity: 0,
@@ -52,6 +60,55 @@ async function textApparitionAnim(titleElem = 'h1', subtitleElem = 'h2', descrip
         stagger: 0.5,
         ease: "elastic.out"
       }, "<1.2")
+      .addLabel("Title Words Animation")
+      .fromTo(".title-char:not(.title-char1):not(.title-char8)", {
+        maxWidth: "50px",
+      } ,{
+        rotateY: 90,
+        opacity: 0,
+        maxWidth: "0px",
+      },"<1")
+      .to(".title-char1", {
+        x: 3,
+        y: -5
+      }, "<")
+      .to(".title-char8", {
+        x: -10,
+        y: 5,
+      }, "<")
+      .addLabel("Subtitle Words Animation")
+      .to(splitSubtitle.words,{
+        rotateX: 90,
+      }, "<0.5")
+      .fromTo(".line1",{
+        rotateX: 90,
+      },{
+        rotateX: 0,
+        rotateZ: 62,
+        x: 10,
+        width: 27,
+        opacity: 1,
+      }, "<0.4")
+      .fromTo(".line2", {
+        rotateX: 90,
+      }, {
+        rotateX: 0,
+        rotateZ: 90,
+        x: -45,
+        width: 35,
+        opacity: 1,
+      }, "<")
+      .to(".line1", {
+        y: -75,
+        x: 63,
+      }, "<")
+      .to(".line2", {
+        y: -49,
+        x: -91,
+      }, "<")
+      .to(".description", {
+        y: -45,
+      }, "<0.2")
       .addLabel("end")
   }
 
