fcb1a703ed01d347540b920a3772dd4bba0c337d
Yannick <yannick.liebnau@gmail.com>
2025-07-12 22:22:31 +0200

    add logo svg && link wireframe

--- src/components/PresentationComponent/PresentationComponent.tsx
+++ src/components/PresentationComponent/PresentationComponent.tsx
@@ -19,13 +19,11 @@ function PresentationComponent() {
  return (
   <div ref={presentationContainer} className="presentation-wrapper flex-grow-1 h-100 relative flex items-center justify-center">
    <div className="presentation h-fit flex flex-col items-center relative">
-    <div className="logo-container text-center">
-     <h1 ref={title} className="!text-h1">Yannick Liebnau</h1>
-     <div className="subtitle-container mb-3 relative">
-      <h2 ref={subtitle} className="subtitle !text-h2">Web Developer</h2>
-      <div className="opacity-0 line line1 w-16 h-px bg-gray-900 inline absolute top-1/2 left-0 translate-y-1/2"></div>
-      <div className="opacity-0 line line2 w-35 h-px bg-gray-900 inline absolute top-1/2 right-0 translate-y-1/2"></div>
-     </div>
+    <h1 ref={title} className="!text-h1">Yannick Liebnau</h1>
+    <div className="subtitle-container mb-3 relative">
+     <h2 ref={subtitle} className="subtitle !text-h2">Web Developer</h2>
+     <div className="opacity-0 line line1 w-16 h-px bg-gray-900 inline absolute top-1/2 left-0 translate-y-1/2"></div>
+     <div className="opacity-0 line line2 w-35 h-px bg-gray-900 inline absolute top-1/2 right-0 translate-y-1/2"></div>
     </div>
     <div ref={description} className="description relative w-100 text-center">
      <p>JavaScript / NodeJS / React</p>
