2b2e7bc0da78a35e3959cf9e4485c274366aa0ce
Yannick <yannick.liebnau@gmail.com>
2025-08-04 08:25:04 +0200

    refactor(projects list): last attempt to observer/scrolltrigger, back to scrollTrigger/timeline

--- src/components/Projects/ProjectsList/ProjectsList.tsx
+++ src/components/Projects/ProjectsList/ProjectsList.tsx
@@ -20,26 +20,106 @@ export default function ProjectsList({ category, id }: ProjectsListProps) {
   useGSAP(() => {
     if (!projectsContainer.current) return;
 
-    const projectsSelector = projectsContainer.current.querySelectorAll(".project")
-    const projects = gsap.utils.toArray<HTMLDivElement>(projectsSelector);
+    const projects = gsap.utils.toArray<HTMLDivElement>('.project');
 
-    gsap.to(projects, {
-      xPercent: -100 * (projects.length),
-      ease: "none",
+    const tl = gsap.timeline({
       scrollTrigger: {
         trigger: projectsContainer.current,
         start: "top top",
-        end: "+=100%",
+        end: `+=100%`,
         scrub: 1,
         pin: true,
         //markers: true,
         snap: {
-          snapTo: 1 / (projects.length),
+          snapTo: 1 / (projects.length - 1),
           inertia: false,
           duration: { min: 0.1, max: 0.3 },
         },
-      },
+      }
+    });
+
+    tl.to(projects, {
+      xPercent: -100 * (projects.length - 1),
+      ease: "none",
+      duration: 5,
     })
+
+    // const wrapper = gsap.utils.wrap(0, projects.length);
+    // let isAnimating = false;
+    // let currentIndex = 0;
+    // let lastIndex = projects.length - 1;
+
+    // gsap.set(projects, { xPercent: -100 });
+    // gsap.set(projects[0], { xPercent: 0 });
+
+    // function slideProjects(index: number, direction: number) {
+    //   isAnimating = true;
+    //   currentIndex = wrapper(index);
+
+    //   const currentProject = projects[currentIndex];
+    //   const nextProject = projects[index];
+
+    //   gsap.set(currentProject, { zIndex: 1, autoAlpha: 1 });
+    //   gsap.set(nextProject, { zIndex: 2, autoAlpha: 1 });
+
+    //   console.log(nextProject);
+
+    //   const tl = gsap.timeline({
+    //     defaults: {
+    //       ease: "power2.inOut",
+    //       duration: 1
+    //     },
+    //     onComplete: () => {
+    //       isAnimating = false;
+    //     }
+    //   });
+
+    //   tl
+    //     .fromTo(currentProject, {
+    //       xPercent: 0,
+    //     }, {
+    //       xPercent: -100 * direction,
+    //     }, 0)
+    //     .fromTo(nextProject, {
+    //       xPercent: 100 * direction,
+    //     }, {
+    //       xPercent: 0,
+    //     }, 0);
+
+    //   currentIndex = index;
+    // }
+
+    // function observe() {
+    //   ScrollTrigger.observe({
+    //     target: projectsContainer.current,
+    //     type: 'wheel,touch',
+    //     onDown: () => {
+    //       if (isAnimating) return;
+    //       if (currentIndex === lastIndex) return;
+    //       slideProjects(currentIndex + 1, +1)
+    //     },
+    //     onUp: () => {
+    //       if (isAnimating) return;
+    //       if (currentIndex === 0) return;
+    //       slideProjects(currentIndex - 1, -1)
+    //     }
+    //   })
+    // }
+
+    // ScrollTrigger.create({
+    //   trigger: projectsContainer.current,
+    //   start: "top top",
+    //   end: `+=100% * ${projects.length * projects.length}`,
+    //   onEnter: () => {
+    //     observe();
+    //   },
+    //   onLeaveBack: () => {
+    //     observe();
+    //   },
+    //   pin: true,
+    //   scrub: 1,
+    //   markers: true,
+    // })
 
   }, { scope: projectsContainer })
 
@@ -55,7 +135,7 @@ export default function ProjectsList({ category, id }: ProjectsListProps) {
   }
 
   return (
-    <div ref={projectsContainer} id={`category-${id}`} className={`projects__${category.toLowerCase()} relative overflow-hidden h-screen`}>
+    <div ref={projectsContainer} id={`category-${id}`} className={`projects__${category.toLowerCase()} overflow-hidden h-screen`}>
       <div className="projects__list h-full flex">
         {getProjectsList(category)}
       </div>
