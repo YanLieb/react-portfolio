256e251d501c54fb45ada187780e877ec4560048
Yannick <yannick.liebnau@gmail.com>
2025-09-27 00:02:34 +0200

    quick add homepage

--- back/src/routes/api.ts
+++ back/src/routes/api.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import type { Request, Response } from 'express';
 
 import projectRouter from './project/project.router'
 import categoryRouter from './category/category.router';
 
 const api = express.Router();
 
+api.get('/', (req: Request, res: Response) => {
+  res.render('index', {title: "Welcome"})
+})
 api.use('/projects', projectRouter)
 api.use('/categories', categoryRouter);
 
