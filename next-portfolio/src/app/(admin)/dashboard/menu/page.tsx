7bbcfeb011e08cdd3cb2c0755c837e8fd6084094
Yannick <yannick.liebnau@gmail.com>
2025-11-04 08:33:17 +0100

    WIP categories

--- next-portfolio/src/app/(admin)/dashboard/menu/page.tsx
+++ next-portfolio/src/app/(admin)/dashboard/menu/page.tsx
@@ -2,11 +2,9 @@ import Link from 'next/link'
 
 export default function Menu() {
   return (
-    <div className="w-40 h-dvh bg-gray-500 p-4">
-      <div className="flex flex-col">
-        <Link href="/dashboard/projects">Projects</Link>
-        <Link href="/dashboard/categories">Categories</Link>
-      </div>
+    <div className="w-40 h-dvh border-r-1 border-gray-200 p-4 flex flex-col gap-2">
+      <Link href="/dashboard/projects">Projects</Link>
+      <Link href="/dashboard/categories">Categories</Link>
     </div>
   )
 }
---
+++ next-portfolio/src/models/Category.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema, model, models } from 'mongoose';
+
+export interface CategoryInterface {
+  title: string;
+  slug: string;
+  description: string;
+  link: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const CategoryModel = new Schema<CategoryInterface>(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please provide a project title'],
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: [true, 'Please provide a project slug'],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Prevent model compilation error in development
+const Category = models.Category || model<CategoryInterface>('Category', CategoryModel);
+
+export default Category;
