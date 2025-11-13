ec0513f1d259e3e983b5b627d5b2d0265e7efca5
Yannick <yannick.liebnau@gmail.com>
2025-11-13 08:09:18 +0100

    controls on login, logout btn

--- next-portfolio/src/app/(auth)/login/page.tsx
+++ next-portfolio/src/app/(auth)/login/page.tsx
@@ -1,8 +1,17 @@
 import { signIn } from 'auth'
 
-export default function LoginPage() {
+export default async function LoginPage({ searchParams }: { searchParams: { error?: string } }) {
+  const params = await searchParams;
+  const error = params.error;
   return (
     <div className='w-full h-dvh flex items-center justify-center flex-col gap-12'>
+      {error === "ServerError" && (
+        <p className="text-red-600">Something went wrong, please try again.</p>
+
+      )}
+      {error === "AccessDenied" && (
+        <p className="text-red-600">You are not allowed to login to this dashboard. Contact your administrator for further informations</p>
+      )}
       <h1 className="text-bold text-3xl">Connexion</h1>
     <form
       action={async () => {
