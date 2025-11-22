ded591cca0878b0f5ede0a98ac9669dbdbddf1c5
Yannick <yannick.liebnau@gmail.com>
2025-11-22 17:58:06 +0100

    WIP front integration

--- next-portfolio/src/app/page.tsx
+++ next-portfolio/src/app/page.tsx
@@ -1,7 +1,47 @@
+"use client"
+
+import { useRef } from 'react';
+
+import { gsap } from 'gsap';
+import { ScrollTrigger, ScrollSmoother } from 'gsap/all';
+import { useGSAP } from '@gsap/react';
+
+import Header from './(front)/components/Header/Header';
+import Hero from './(front)/components/Hero/Hero';
+import ProjectsList from './(front)/components/Projects/ProjectsList';
+import Contact from './(front)/components/Contact/Contact';
+import Bubbles from "./(front)/components/Projects/inc/Bubbles";
+
+import "./front.css";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
 export default function Home() {
+  const mainContainer = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 1.5,
+      effects: true,
+      smoothTouch: 0.2
+    })
+  }, { scope: mainContainer })
+
   return (
-    <div className="">
-      
+    <div ref={mainContainer} id="smooth-wrapper">
+      <Header />
+      <div id="smooth-content" className="home relative">
+        <Bubbles
+          className="cursor-anim-wrapper"
+          fade={30}
+          strokeGray={230}
+          bg={255}
+        >
+          <Hero />
+          <ProjectsList />
+          <Contact />
+        </Bubbles>
+      </div>
     </div>
-  );
+  )
 }
