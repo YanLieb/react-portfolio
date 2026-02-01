27b6be1b54b0181b7485929c7c8b32f1a3448107
Yannick <yannick.liebnau@gmail.com>
2026-02-01 16:12:30 +0100

    clean repo, separating back and next on other repo

--- back/src/models/category/category.mongo.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const { Schema, model, Type } = mongoose;
-
-const categorySchema = new Schema({
-  name: { type: String, required: true },
-  slug: { type: String, required: true },
-})
-
-export default model('Category', categorySchema);
